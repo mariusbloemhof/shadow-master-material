0bf6931866e9fb2601010264bf10a217393bb84e
Marius Bloemhof <mariusbloemhof@gmail.com>
2016-03-07 21:05:39 +0200

    !added BLE connectivity to app

--- app/src/main/java/za/co/shadow/material/activity/LinkDeviceFragment.java
+++ app/src/main/java/za/co/shadow/material/activity/LinkDeviceFragment.java
@@ -1,6 +1,7 @@
 package za.co.shadow.material.activity;
 
 
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,8 +12,11 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -23,9 +27,18 @@ import java.util.Arrays;
 import java.util.List;
 
 import za.co.shadow.Parse.ShadowDevice;
+import za.co.shadow.blelib.ble.BLEDeviceListAdapter;
+import za.co.shadow.blelib.ble.BluetoothHandler;
 import za.co.shadow.material.R;
 
 public class LinkDeviceFragment extends Fragment {
+
+
+    private Button scanButton;
+    private ListView bleDeviceListView;
+    private BLEDeviceListAdapter listViewAdapter;
+    private BluetoothHandler bluetoothHandler;
+    private boolean isConnected;
 
     public LinkDeviceFragment() {
         // Required empty public constructor
@@ -42,35 +55,69 @@ public class LinkDeviceFragment extends Fragment {
                              Bundle savedInstanceState)
     {
 
-        View view = inflater.inflate(R.layout.fragment_linkdevice, container, false);
+        final View view = inflater.inflate(R.layout.fragment_linkdevice, container, false);
 
-        Button btnScanDevice = (Button)  view.findViewById(R.id.btnScan_shadow);
-        btnScanDevice.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                //scan for device
-                if (ScanforDevice()) {
-                    ShadowDevice shadowdevice;
-                    ParseUser user = ParseUser.getCurrentUser();
-                    ParseQuery<ShadowDevice> query = ParseQuery.getQuery("ShadowDevice");
-                    query.whereEqualTo("user", user);
-                    try {
-                        List<ShadowDevice> results = query.find();
-                        if (results.size() > 0) {
-                            shadowdevice = results.get(0);
-                        }
-                        else {
-                            shadowdevice = new ShadowDevice();
-                        }
-                    }
-                    catch (ParseException e) {shadowdevice = new ShadowDevice();}
-                    shadowdevice.setBlueToothDeviceID(21154);
-                    shadowdevice.SaveLater();
+//        Button btnScanDevice = (Button)  view.findViewById(R.id.btnScan_shadow);
+//        btnScanDevice.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//            //scan for device
+//            if (ScanforDevice()) {
+//                ShadowDevice shadowdevice;
+//                ParseUser user = ParseUser.getCurrentUser();
+//                ParseQuery<ShadowDevice> query = ParseQuery.getQuery("ShadowDevice");
+//                query.whereEqualTo("user", user);
+//                try {
+//                    List<ShadowDevice> results = query.find();
+//                    if (results.size() > 0) {
+//                        shadowdevice = results.get(0);
+//                    }
+//                    else {
+//                        shadowdevice = new ShadowDevice();
+//                    }
+//                }
+//                catch (ParseException e) {shadowdevice = new ShadowDevice();}
+//                shadowdevice.setBlueToothDeviceID(21154);
+//                shadowdevice.SaveLater();
+//            }
+//            }
+//        });
 
-                }
-            }
+        scanButton = (Button) view.findViewById(R.id.btnScan_shadow);
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+                                             public void onClick(View v) {
+                                                 scanOnClick(v);
+                                             }
         });
 
+
+        bleDeviceListView = (ListView) view.findViewById(R.id.bleDeviceListView);
+        listViewAdapter = new BLEDeviceListAdapter(this.getActivity());
+
+        bluetoothHandler = new BluetoothHandler(this.getActivity());
+        bluetoothHandler.setOnConnectedListener(new BluetoothHandler.OnConnectedListener() {
+
+            @Override
+            public void onConnected(boolean isConnected) {
+                // TODO Auto-generated method stub
+                setConnectStatus(isConnected);
+            }
+        });
+//        bluetoothHandler.connect("D0:39:72:BF:70:F3");
+
         return view;
+    }
+
+    public void setConnectStatus(boolean isConnected){
+        this.isConnected = isConnected;
+        if(isConnected){
+            showMessage("Connection successful");
+            scanButton.setText("break");
+        }else{
+            bluetoothHandler.onPause();
+            bluetoothHandler.onDestroy();
+            scanButton.setText("Scan for Shadow remote");
+        }
     }
 
     @Override
@@ -90,6 +137,46 @@ public class LinkDeviceFragment extends Fragment {
 
     private Boolean ScanforDevice() {
         return true;
+    }
+
+    private void showMessage(String str){
+        Toast.makeText(this.getActivity(), str, Toast.LENGTH_SHORT).show();
+    }
+
+    public void scanOnClick(final View v){
+        if(!isConnected){
+            bleDeviceListView.setAdapter(bluetoothHandler.getDeviceListAdapter());
+            bleDeviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    String buttonText = (String) ((Button)v).getText();
+                    if(buttonText.equals("scanning")){
+                        showMessage("scanning...");
+                        return ;
+                    }
+                    BluetoothDevice device = bluetoothHandler.getDeviceListAdapter().getItem(position).device;
+                    // connect
+                    bluetoothHandler.connect(device.getAddress());
+                }
+            });
+            bluetoothHandler.setOnScanListener(new BluetoothHandler.OnScanListener() {
+                @Override
+                public void onScanFinished() {
+                    // TODO Auto-generated method stub
+                    ((Button)v).setText("scan");
+                    ((Button)v).setEnabled(true);
+                }
+                @Override
+                public void onScan(BluetoothDevice device, int rssi, byte[] scanRecord) {}
+            });
+            ((Button)v).setText("scanning");
+            ((Button)v).setEnabled(false);
+            bluetoothHandler.scanLeDevice(true);
+        }else{
+            setConnectStatus(false);
+        }
     }
 
 }
